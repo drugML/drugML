85877a056b01b21fec512aacf947e0c95a02566c
yanchen01 <ychen01@bu.edu>
2020-12-24 15:27:16 -0500

    added scraper for drugs, processed raw data to clean csv

--- src/backend/scraper/main.py
+++ src/backend/scraper/main.py
@@ -1,1 +1,81 @@
-PUBG_CHEM_URL = "https://pubchem.ncbi.nlm.nih.gov/#query=acebutolol"
+from scraper import Scraper
+import csv
+
+
+def read_file(file_path, names_list):
+    f = open(file_path, mode='r', encoding='utf-8')
+    try:
+        for line in f:
+            drug_name, CID = line.split(',')
+            names_list.append([drug_name.lower().strip(), CID.lower().strip()])
+    except Exception:
+        print(Exception)
+
+
+def get_drug_names_from_file(drugs_work, drugs_dont_work):
+    """Function to append names of drug to a list
+
+    Args:
+        drugs_work (list): list of drugs that work
+        drugs_dont_work (list): list of drugs that don't work
+    """
+
+    read_file("./files/drugs_that_work.txt", drugs_work)
+    # read_file("./files/drugs_that_dont_work.txt", drugs_dont_work)
+
+
+def scrape_drugs_work_property(drugs_work):
+    # for each drug, instantiate a scraper instance
+    for index, (drug_name, CID) in enumerate(drugs_work):
+        drug = Scraper(drug_name, CID, True)
+        raw_properties = drug.get_properties()
+        drug.format_properties(raw_properties)
+        # replace name with scraper instance
+        drugs_work[index] = drug
+
+
+def write_to_csv(file_path, drugs):
+    field_names = [
+        'name',
+        'cid',
+        'weight',
+        'hydro_bond_donor_count',
+        'hydro_bond_acceptor_count',
+        'topo_polar_surface_area',
+        'heavy_atom_count',
+        'formal_charge',
+        'complexity',
+        'melting_pt',
+        'boiling_pt',
+        'solubility',
+        'density',
+        'pH',
+        'pKa',
+        'dissociation_const',
+        'collision_cross_sec',
+        'work'
+    ]
+
+    result_data = []
+    for drug in drugs:
+        result_data.append(drug.properties)
+
+    with open(file_path, 'w', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(result_data)
+
+
+def main():
+    drugs_work = []
+    drugs_dont_work = []
+    # get all names from file to list
+    get_drug_names_from_file(drugs_work, drugs_dont_work)
+    # for all names, scrape its properties
+    scrape_drugs_work_property(drugs_work)
+    # write all scraped data to csv
+    write_to_csv('./files/sample_data.csv', drugs_work)
+
+
+if __name__ == "__main__":
+    main()
