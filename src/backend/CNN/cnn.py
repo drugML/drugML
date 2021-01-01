5ac8bc7cb9c9e4f0eab39dec138c70e0d2296cc4
Benjamin Li <pharmashawarma@gmail.com>
2021-01-01 14:44:29 -0500

    refactoring

--- src/backend/CNN/cnn.py
+++ src/backend/CNN/cnn.py
@@ -4,21 +4,30 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import plot_model
 from numpy import loadtxt
 
+def split_data(dataset):
+    x = dataset[:,1:11]
+    y = dataset[:,11]
+    return x, y
+
+def model_setup():
+    model = Sequential()
+    model.add(Dense(64, input_dim=10, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    return model
+
+
 # load training dataset from directory
 dataset = loadtxt(open(r"..\..\..\dataset\hypertension\training_0.0.1.csv", "rb"), delimiter=",", skiprows=1)
 
-x = dataset[:,1:11]
-y = dataset[:,11]
+x, y = split_data(dataset)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=10)
 
-
-model = Sequential()
-model.add(Dense(64, input_dim=10, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+# TODO add params for layer size
+model = model_setup()
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
